f24f13787865377451b5ca227ee9204bb9d47289
Felipe Rodrigues Farias <feliperfariasdev@gmail.com>
2023-02-14 20:22:17 -0300

    README.md & package.json update server,js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from 'express'
 const app = express()
-const PORT = 3000
 
 app.get('/', (request, response) => {
   response.send('Olá Mundo!')
 })
 
-app.listen(PORT, () => {
-  console.log(`Aplicativo de exemplo ouvindo em http://localhost:${PORT}/`)
-})
+export default app
